eb1a121e84ec66ac4e7ef288d1c733a46767d9d2
tom-segsu <tom@segev.com>
2021-04-11 16:33:02 +1000

    fixed piece representation

--- constants.ts
+++ constants.ts
@@ -1,6 +1,6 @@
 import { newGameBoard } from "./collision";
-import { GameState } from "./types";
-import { randomPiece } from "./Tetris";
+import { GameState, Piece, PieceName } from "./types";
+import { randomPieces } from "./random";
 
 export const KeyBindings = {
 	left: 85,
@@ -12,16 +12,6 @@ export const KeyBindings = {
 	hold: 68,
 	reset: 82
 };
-
-export const pieces: string[] = [
-	"O_PIECE",
-	"I_PIECE",
-	"J_PIECE",
-	"L_PIECE",
-	"S_PIECE",
-	"Z_PIECE",
-	"T_PIECE"
-];
 
 export const CELL = {
 	EMPTY: 0,
@@ -48,19 +38,21 @@ export const COLOURSCHEME = [
 export const PIECE = {
 	O_PIECE: {
 		id: 1,
+		name: "O_PIECE",
 		colour: COLOURSCHEME[1],
-		rotationalCentre: { x: 0.5, y: 0.5 },
+		rotationalCentre: { x: 0.5, y: -0.5 },
 		shape: [
 			{ x: 0, y: 0 },
-			{ x: 0, y: 1 },
+			{ x: 0, y: -1 },
 			{ x: 1, y: 0 },
-			{ x: 1, y: 1 }
+			{ x: 1, y: -1 }
 		]
 	},
 	I_PIECE: {
 		id: 2,
+		name: "I_PIECE",
 		colour: COLOURSCHEME[2],
-		rotationalCentre: { x: 1.5, y: 0.5 },
+		rotationalCentre: { x: 1.5, y: -0.5 },
 		shape: [
 			{ x: 0, y: 0 },
 			{ x: 1, y: 0 },
@@ -70,10 +62,11 @@ export const PIECE = {
 	},
 	J_PIECE: {
 		id: 3,
+		name: "J_PIECE",
 		colour: COLOURSCHEME[3],
 		rotationalCentre: { x: 1, y: 0 },
 		shape: [
-			{ x: 0, y: 1 },
+			{ x: 0, y: -1 },
 			{ x: 0, y: 0 },
 			{ x: 1, y: 0 },
 			{ x: 2, y: 0 }
@@ -81,54 +74,69 @@ export const PIECE = {
 	},
 	L_PIECE: {
 		id: 4,
+		name: "L_PIECE",
 		colour: COLOURSCHEME[4],
 		rotationalCentre: { x: 1, y: 0 },
 		shape: [
 			{ x: 0, y: 0 },
 			{ x: 1, y: 0 },
 			{ x: 2, y: 0 },
-			{ x: 2, y: 1 }
+			{ x: 2, y: -1 }
 		]
 	},
 	S_PIECE: {
 		id: 5,
+		name: "S_PIECE",
 		colour: COLOURSCHEME[5],
 		rotationalCentre: { x: 1, y: 0 },
 		shape: [
 			{ x: 0, y: 0 },
 			{ x: 1, y: 0 },
-			{ x: 1, y: 1 },
-			{ x: 2, y: 1 }
+			{ x: 1, y: -1 },
+			{ x: 2, y: -1 }
 		]
 	},
 	Z_PIECE: {
 		id: 6,
+		name: "Z_PIECE",
 		colour: COLOURSCHEME[6],
 		rotationalCentre: { x: 1, y: 0 },
 		shape: [
-			{ x: 0, y: 1 },
-			{ x: 1, y: 1 },
+			{ x: 0, y: -1 },
+			{ x: 1, y: -1 },
 			{ x: 1, y: 0 },
 			{ x: 2, y: 0 }
 		]
 	},
 	T_PIECE: {
 		id: 7,
+		name: "T_PIECE",
 		colour: COLOURSCHEME[7],
 		rotationalCentre: { x: 1, y: 0 },
 		shape: [
 			{ x: 0, y: 0 },
 			{ x: 1, y: 0 },
-			{ x: 1, y: 1 },
+			{ x: 1, y: -1 },
 			{ x: 2, y: 0 }
 		]
 	}
 };
 
+export const pieces: Piece[] = [
+	PIECE.I_PIECE,
+	PIECE.J_PIECE,
+	PIECE.L_PIECE,
+	PIECE.O_PIECE,
+	PIECE.S_PIECE,
+	PIECE.T_PIECE,
+	PIECE.Z_PIECE
+];
+
 export const STARTINGPOS = { x: 3, y: -1 };
 
 // whole game state
 export const NewGameState: GameState = {
+	queue: randomPieces(5),
 	cummulativeLineClears: 10,
 	level: 0,
 	score: 0,
