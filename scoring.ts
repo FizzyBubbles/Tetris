7fb02e189f8eacf47521c5402afaecdc913674ff
tom-segsu <tom@segev.com>
2021-04-11 14:51:02 +1000

    fixes scoring algorithm

--- scoring.ts
+++ scoring.ts
@@ -1,13 +1,13 @@
 export const calculateScore = (numLinesCleared: number) => (level: number) => {
 	const scoreTable = [40, 100, 300, 1200];
-	console.log(
-		"score: ",
-		numLinesCleared > 0 ? (level + 1) * scoreTable[numLinesCleared - 1] : 0
-	);
+	// console.log(
+	// 	"score: ",
+	// 	numLinesCleared > 0 ? (level + 1) * scoreTable[numLinesCleared - 1] : 0
+	// );
 	return numLinesCleared > 0
 		? (level + 1) * scoreTable[numLinesCleared - 1]
 		: 0;
 };
 
 export const calculateLevel = (numLinesCleared: number) =>
-	Math.floor(Math.sqrt(25 + 20 * numLinesCleared) - 5) / 10;
+	Math.floor((Math.sqrt(25 + 20 * numLinesCleared) - 5) / 10);
