623182f44d4ab03a005190190da9806067858765
tom-segsu <tom@segev.com>
2021-05-25 10:27:53 +1000

    scoring tests and comments completed

--- tests/scoring.test.ts
+++ tests/scoring.test.ts
@@ -1,4 +1,4 @@
-import { calculateScore } from "../tetris_modules/scoring";
+import { calculateScore, calculateLevel } from "../tetris_modules/scoring";
 
 describe("score", () => {
 	it("should return the proper score", () => {
@@ -9,5 +9,13 @@ describe("score", () => {
 	});
 	it("if negative or zero line clears should return 0", () => {
 		expect(calculateScore(-8)(2)).toStrictEqual(0);
+	});
+});
+describe("level", () => {
+	it("should return the proper level", () => {
+		expect(calculateLevel(100)).toStrictEqual(4);
+	});
+	it("if negative or zero line clears should return 0", () => {
+		expect(calculateLevel(-8)).toStrictEqual(0);
 	});
 });
--- tetris_modules/scoring.ts
+++ tetris_modules/scoring.ts
@@ -1,9 +1,11 @@
 export const calculateScore = (numLinesCleared: number) => (level: number) => {
 	const scoreTable = [40, 100, 300, 1200]; // base score for line clears in order from 1-4
-	return numLinesCleared > 0 // only returns a non zero score if more than 0 lines are cleared
+	return numLinesCleared > 0 // only returns a non-zero score if more than 0 lines are cleared
 		? (level + 1) * scoreTable[(numLinesCleared - 1) % 4] // the score is calculated based on the standardised score calculations
 		: 0; // score is 0 if 0 or less lines cleared
 };
 
 export const calculateLevel = (numLinesCleared: number) =>
-	Math.floor((Math.sqrt(25 + 20 * numLinesCleared) - 5) / 10); // mathematical wizardry (when 10 is added next level when 20 is added next level etc.)
+	numLinesCleared > 0 // only returns a non-zero level if more than 0 lines cleared
+		? Math.floor((Math.sqrt(25 + 20 * numLinesCleared) - 5) / 10) // mathematical wizardry (when 10 is added next level when 20 is added next level etc.)
+		: 0; // level is 0 if 0 or less lines cleared
