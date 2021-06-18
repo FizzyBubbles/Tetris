8b999ea1a3803be7b959e2f368c3602952fc998d
tom-segsu <tom@segev.com>
2021-06-18 15:03:00 +1000

    completely stabilised version of tetris

--- Tetris.ts
+++ Tetris.ts
@@ -330,12 +330,3 @@ document.onkeydown = e => {
 };
 
 main();
-
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-	document.getElementById("sideMenu").style.width = "250px";
-	document.getElementById("container").style.marginLeft = "250px";
-	document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-}
-
-document.getElementById("settings").addEventListener("click", openNav);
